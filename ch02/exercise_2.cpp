b7ccbb467be4b548ca04d5dc96dffcba9ad0ad72
HFJ-D <62201534+HFJ-D@users.noreply.github.com>
2022-04-29 16:24:45 +0800

    Add files via upload

---
+++ ch02/exercise_2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>;
+
+
+int main()
+{
+	using namespace std;
+	int longs;
+	cout << "Enter a distance in long unit: ";
+	cin >> longs;
+	cout << longs << " long = " << 220*longs << " yard.\n";
+
+	return 0;
+}
