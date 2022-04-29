af4ddde109c40dfd2d494153abb52ac78a844941
HFJ-D <62201534+HFJ-D@users.noreply.github.com>
2022-04-29 15:49:45 +0800

    Add files via upload

---
+++ ch02/exercise_4.cpp
@@ -0,0 +1,11 @@
+#include <iostream>;
+
+int main()
+{
+	using namespace std;
+	int age;
+	cout << "Enter your age: ";
+	cin >> age;
+	cout << "You have lived " << age * 12 << " months.\n";
+	return 0;
+}
