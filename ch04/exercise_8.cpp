9919976da4502d49518b04efbaeb7890fa92627e
HFJ-D <62201534+HFJ-D@users.noreply.github.com>
2022-05-09 20:28:38 +0800

    Add files via upload

---
+++ ch04/exercise_8.cpp
@@ -0,0 +1,27 @@
+#include <iostream>;
+#include <string>
+using namespace std;
+
+struct Pizza
+{
+    string name;
+    double diameter;
+    double weight;
+};
+
+int main()
+{
+    Pizza *sample = new Pizza;
+    cout << "Enter the diameters of the pizza: ";
+    cin >> sample->diameter;
+    cout << "Enter the name of pizza company: ";
+    cin.get();
+    getline(cin, sample->name);
+    cout << "Enter the weight of the pizza: ";
+    cin >> sample->weight;
+
+    cout << "The pizza is of " << sample->name << " company.\n" 
+         << "The diameters of the pizza is " << sample->diameter 
+         << " and the weight is " << sample->weight << ".\n";
+    return 0;
+}
