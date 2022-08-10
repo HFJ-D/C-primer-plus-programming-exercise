4b51ce64a8a8b83058aa61986fc076732c3bae4c
HFJ-D <62201534+HFJ-D@users.noreply.github.com>
2022-08-10 19:31:10 +0800

    Add files via upload

---
+++ ch12/exercise_3/stock.cpp
@@ -0,0 +1,100 @@
+
+#include "stock.h"
+#include <cstring>
+
+Stock::Stock()
+{
+	company = "no name";
+	shares = 0;
+	share_val = 0.0;
+	total_val = 0.0;
+}
+
+
+Stock::Stock(const char * co, long n, double pr)
+{
+	company = new char[strlen(co)+1];
+	strcpy(company, co);
+
+	if(n < 0)
+	{
+		std::cout << "Numer of shares can't be negative; " << company << " shares set to be 0." << std::endl;
+		shares = 0;
+	}	
+	else
+		shares = n;
+
+	share_val = pr;
+
+	set_tot();
+}
+
+Stock::~Stock()
+{
+	delete []company;
+}
+
+void Stock::buy(long num, double price)
+{
+	if(num < 0)
+	{
+		std::cout << "Numer of shares can't be negative, Transaction is aborted" << std::endl;
+	}
+	else
+	{
+		shares += num;
+		share_val = price;
+		set_tot();
+	}
+}
+
+void Stock::sell(long num, double price)
+{
+	using std::cout;
+
+	if(num < 0)
+	{
+		std::cout << "Numer of shares can't be negative, Transaction is aborted" << std::endl;
+	}
+	else if(num > shares)
+	{
+		cout << "You can't sell more than you have! Transaction is aborted." << std::endl;
+	}
+	else
+	{
+		shares -= num;
+		share_val = price;
+		set_tot();
+	}
+}
+
+void Stock::update(double price)
+{
+	share_val = price;
+	set_tot();
+}
+
+ostream & operator<<(ostream & os, const Stock & s)
+{
+	using std::ios_base;
+	ios_base::fmtflags orig = os.setf(ios_base::fixed, ios_base::floatfield);
+	std::streamsize prec = os.precision(3);
+
+	os << "Company: " << s.company
+	   << " Shares: " << s.shares << '\n';
+	os << " Share price: $" << s.share_val;
+	os.precision(2);
+	os <<" Total Worth: $" << s.total_val << '\n';
+	os.setf(orig, ios_base::floatfield);
+	os.precision(prec);
+
+	return os;
+}
+
+const Stock & Stock::topval(const Stock &s) const
+{
+	if(s.total_val > total_val)  //total_val == this->total_val
+		return s;
+	else
+		return *this;
+}
---
+++ ch12/exercise_3/stock.h
@@ -0,0 +1,26 @@
+#ifndef STOCK_H
+#define STOCK_H
+#include <iostream>
+using std::ostream;
+
+class Stock
+{
+	private:
+		char * company;
+		long shares;
+		double share_val;
+		double total_val;
+		void set_tot() {total_val = shares * share_val;}
+	public:
+		Stock();
+		Stock(const char * co, long n = 0, double pr = 0.0);
+		~Stock();
+		void buy(long num, double price);
+		void sell(long num, double price);
+		void update(double price);
+		friend ostream & operator<<(ostream & os, const Stock & s);
+		const Stock &topval(const Stock &s) const;
+};
+
+
+#endif
