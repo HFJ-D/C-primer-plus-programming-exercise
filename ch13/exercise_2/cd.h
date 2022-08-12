625bf33ac0d5da1fa2bd133b5eeb367b1c75309c
HFJ-D <62201534+HFJ-D@users.noreply.github.com>
2022-08-12 20:46:41 +0800

    Add files via upload

---
+++ ch13/exercise_2/cd.h
@@ -0,0 +1,33 @@
+#ifndef CD_H
+#define CD_H
+#include <iostream>
+class Cd{
+    private:
+        char * performers;
+        char * label;
+        int selections;
+        double playtime;
+    public:
+        Cd(const char * s1, const char * s2, int n, double x);
+        Cd(const Cd & d);
+        Cd();
+        virtual ~Cd(){delete [] performers; delete [] label;}
+        virtual void Report() const;
+        Cd & operator=(const Cd & c);
+};
+
+class Classic: public Cd
+{
+    private:
+        char * works;
+    public:
+        Classic();
+        Classic(const char * work, const char * s1, const char * s2, int n, double x);
+        Classic(const Cd & c, const char * work = "C++");
+        Classic(const Classic & c);
+        virtual ~Classic(){ delete [] works;}
+        virtual void Report() const;
+        Classic & operator=(const Classic & d);
+};
+
+#endif
